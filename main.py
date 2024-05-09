3252395351e9b009b96ad62302bb29646f548188
Lefteris Gilmaz <lefterisgilmaz@Lefteriss-MacBook-Pro.local>
2024-05-09 14:05:50 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from tabula import read_pdf
+import pandas as pd
+import re
+import numpy as np
+
+
+class Santander:
+
+    
+    def get_data_from_santander(self, input_file, output_file):
+        try:
+            df_page2 = read_pdf(input_file, pages=2, area=[399.99, 0, 842, 595], multiple_tables=True, pandas_options={'header': None})
+            df_page3 = read_pdf(input_file, pages=3, multiple_tables=True, pandas_options={'header': None})
+            all_tables = df_page2 + df_page3
+            combined_df = pd.concat(all_tables, ignore_index=True)
+            return combined_df.to_csv(output_file, index=False)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    
+
+class DataCleaning:
+
+    
+    def clean_santander_data(self, table):
+        df = pd.DataFrame(table)
+        df = df[3:] 
+        df.columns = ['Date', 'Description', 'Money in', 'Money out', 'Balance']
+        df['Money in'] = df['Description']  
+        df['Description'] = df['Date'].str.extract(r'(\d+\w+ \w+ [A-Z]+[^\d]+)') 
+        df['Date'] = df['Date'].str.extract(r'(\d+\w+ \w+)')
+        rows_to_drop = [31] 
+        df.drop(index=rows_to_drop, inplace=True)
+        condition = df['Money in'].apply(lambda x: isinstance(x, str) and x.isdigit() == False)
+        df.loc[condition, 'Description'] = df.loc[condition, 'Money in']
+        df.loc[condition, 'Money in'] = np.nan
+        df['Money in'] = pd.to_numeric(df['Money in'], errors='coerce')
+        return df
+
+        
+
+
+
+
+if __name__ == '__main__':
+    sa = Santander()
+    dc = DataCleaning()
+
+
+    table1 = sa.get_data_from_santander('file1.pdf', 'o1.csv')
+    table = pd.read_csv('o1.csv')
+    table2 = dc.clean_santander_data(table)
+    print(table2.head(60))
+    
+
+    
+
+
+    
+    
+
